59e17aa04e967992edd5af9d32529d819d592759
sinwindis <sinwindis@naver.com>
2020-11-11 20:53:14 +0900

    SYSTEM ENHANCEMENT LoadActivity loading data and showing screen processed using thread

--- app/src/main/java/com/example/nemologic/data/DbManager.java
+++ app/src/main/java/com/example/nemologic/data/DbManager.java
@@ -321,7 +321,7 @@ public class DbManager {
 
 
 
-        Log.d("DBManager", "load end");
+        Log.d("DBManager", "loadData end");
 
         return currentVersion;
     }
--- app/src/main/java/com/example/nemologic/loadactivity/LoadActivity.java
+++ app/src/main/java/com/example/nemologic/loadactivity/LoadActivity.java
@@ -1,9 +1,16 @@
 package com.example.nemologic.loadactivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.nemologic.R;
@@ -16,44 +23,47 @@ import java.sql.SQLException;
 
 public class LoadActivity extends AppCompatActivity {
 
-    private void load()
-    {
-        DbOpenHelper mDbOpenHelper = new DbOpenHelper(this);
-        try {
-            mDbOpenHelper.open();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        mDbOpenHelper.create();
-        mDbOpenHelper.close();
-
-        DbManager dbManger = new DbManager(this);
-
-        dbManger.loadData();
-        StringGetter.loadData(this);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load);
 
+        final Handler loadHandler = new Handler();
+        final Handler intentHandler = new Handler();
 
-
-
-
-        Handler loadHandler = new Handler();
-
-        Handler mainHandler = new Handler();
-
-
-        loadHandler.post(new Runnable() {
+        runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                load();
+                setContentView(R.layout.activity_load);
 
-                Intent intent = new Intent(LoadActivity.this, MainActivity.class);
-                startActivity(intent);
+                loadHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Context ctx = getBaseContext();
+
+                        DbOpenHelper mDbOpenHelper = new DbOpenHelper(ctx);
+                        try {
+                            mDbOpenHelper.open();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        mDbOpenHelper.create();
+                        mDbOpenHelper.close();
+
+                        DbManager dbManger = new DbManager(ctx);
+
+                        dbManger.loadData();
+                        StringGetter.loadData(ctx);
+
+                        intentHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent = new Intent(LoadActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                });
             }
         });
     }
